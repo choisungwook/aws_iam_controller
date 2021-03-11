56e26ab6864ec110a7c5005cb3ec5e1def85f3ea
choisungwook <kgg1959@naver.com>
2021-03-11 23:10:49 +0900

    feats: 회원가입 기능

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,29 @@
 import json
 import boto3
 
+
+
+"""
+    (todo) 예외처리 필요
+    1. 중복
+    2. 권한(IAM) 에러
+    3. 등등
+"""
+def create_user(username):
+    iam_client = boto3.resource('iam')
+    user = iam_client.create_user(username)
+
 """
     main function
 """
 def lambda_handler(event, context):
+    # (todo)예외 처리 필요 -> 파싱오류
     username_parameter = event['queryStringParameters']['username']
 
+    create_user(username_parameter)
+    # (todo) 회원가입 실패하면 에러 응답 전달
     statusCode = 200
+
     response_body = {
         "hello": "world"
     }
