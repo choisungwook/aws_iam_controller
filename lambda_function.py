ef9adc0026c201dba09cdbf9ae29fc50e901effc
choisungwook <kgg1959@naver.com>
2021-03-11 23:33:55 +0900

    feats: 명령어 파싱

--- lambda_function.py
+++ lambda_function.py
@@ -27,6 +27,14 @@ def create_user_with_policy(username, policy):
     pass
 
 """
+    IAM 계정 삭제
+    (todo) 예외처리 
+"""
+def delete_user(username):
+    iam_client = boto3.resource('iam')
+    iam_client.delete_user(UserName=username)
+
+"""
     IAM 유저 목록
 """
 def list_user():
@@ -36,14 +44,18 @@ def list_user():
     for x in response['Users']:
         print(x['UserName'])
 
+
 """
     main function
 """
 def lambda_handler(event, context):
     # (todo)예외 처리 필요 -> 파싱오류
-    username_parameter = event['queryStringParameters']['username']
-
-    create_user(username_parameter)
+    
+    request_parameter = event['queryStringParameters']
+    function_name = request_parameter['command']
+    
+    globals()[function_name](event['queryStringParameters'])
+    
     # (todo) 회원가입 실패하면 에러 응답 전달
     statusCode = 200
 
