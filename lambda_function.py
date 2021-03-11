1f76bcb67baa2a8e2230a482ef99b904ed3cb855
choisungwook <kgg1959@naver.com>
2021-03-11 22:18:46 +0900

    프로젝트 생성

---
+++ lambda_function.py
@@ -0,0 +1,23 @@
+import json
+import botot3
+
+"""
+    main function
+"""
+def lambda_handler(event, context):
+    username_parameter = event['username']
+
+    statusCode = 200
+    response_body = {
+        "hello": "world"
+    }
+
+    return {
+        'statusCode': statusCode,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
